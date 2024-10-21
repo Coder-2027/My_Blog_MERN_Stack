8274f3e492bd0d9eef76c7ac3c3368b84b8b4945
Coder-2027 <codingninja2027@gmail.com>
2024-10-21 08:06:39 +0530

    Add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -57,7 +57,7 @@ export const signin = async (req, res, next) => {
     }
 
     const token = jwt.sign(
-      { userId: validUser._id },
+      { userId: validUser._id, isAdmin: validUser.isAdmin },
       process.env.JWT_SECRET_KEY,
       { expiresIn: "1d" }
     );
@@ -82,7 +82,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET_KEY, {
         expiresIn: "1d",
       });
       const { password, ...rest } = user._doc;
@@ -100,7 +100,7 @@ export const google = async (req, res, next) => {
         profilePicture,
       });
       // await newUser.save();
-      const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY, {
+      const token = jwt.sign({ userId: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET_KEY, {
         expiresIn: "1d",
       });
       const { password,...rest } = newUser._doc;
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,11 @@ const userSchema = new mongoose.Schema({
     profilePicture: {
         type: String,
         default: 'https://img.freepik.com/premium-photo/memoji-emoji-handsome-smiling-man-white-background_826801-6987.jpg?w=740'
-    }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
 }, {timestamps: true});
 
 export const User = mongoose.model('User', userSchema);
