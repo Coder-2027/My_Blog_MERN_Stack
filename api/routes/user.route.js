ce96ab0ffd0868fa819048d3f4156b4c999c8f60
Coder-2027 <codingninja2027@gmail.com>
2024-10-23 09:21:19 +0530

    Add get All users API

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { updateUser, deleteUser, signout } from '../controllers/user.controller.js';
+import { updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.route('/update/:userId').put(verifyToken, updateUser);
 router.route('/delete/:userId').delete(verifyToken, deleteUser);
 router.route('/signout').post(signout);
+router.route('/getUsers').post(verifyToken, getUsers);
 
 export default router;
